daff7041c10eb0dafa9a906622972694b8848153
Linh B Ngo <linh.b.ngo@gmail.com>
2018-10-29 11:18:59 -0400

    sql injection draft

--- source/find_myshell.c
+++ source/find_myshell.c
@@ -3,6 +3,9 @@
 int main()
 {
   char *shell = (char *) getenv("MYSHELL");
+  printf ("Test testing \n");
+  int x, y; 
+  printf ("%d\n", x + y);
   if (shell) {
     printf ("Value: %s\n", shell);
     printf ("Address: %x\n", (unsigned int)shell); 
