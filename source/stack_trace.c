bfec3e1ac0b6c43f4ff6bd1cd2a5cf21c7b57507
Linh B Ngo <lngo@wcupa.edu>
2019-09-27 12:51:20 -0400

    Update stack_trace.c

--- source/stack_trace.c
+++ source/stack_trace.c
@@ -2,11 +2,9 @@
 static void display(int i, int *ptr);
     
 int main(void) {
- int x;
- int *xptr;
+ int x = 5;
+ int *xptr = 5;
  printf("In main():\n");
- x = 5;
- xptr = &x;
  printf("   x is %d and is stored at %p.\n", x, &x);
  printf("   xptr points to %p which holds %d.\n", xptr, *xptr);
  display(x, xptr);
@@ -15,10 +13,7 @@ int main(void) {
 }
    
 void display(int z, int *zptr) {
-  int y;
   printf("In display():\n");
-  y = 10;
   printf("   z is %d and is stored at %p.\n", z, &z);
   printf("   zptr points to %p which holds %d.\n", zptr, *zptr);
-  *zptr = 16; 
 }
